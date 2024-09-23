53a9b6179f063a347fb9eeb68e89ac2d3aa5adf9
Katie Gyte <kgyte@student.ubc.ca>
2024-09-22 21:59:07 -0700

    R scripts

---
+++ 01_scripts/data_processing.R
@@ -0,0 +1,72 @@
+#### Cleaning raw data
+
+#load libraries
+library(tidyr)
+library(ggplot2)
+
+#read in rawdata file
+D1data <- read.csv("00_rawdata/D1data.csv")
+
+####Only import select columns from raw data file####
+#Define your columns_to_import function
+columns_to_import <- function(df) {
+  selected_columns <- df[, c("filename", "Particle.ID", "Convex.Perimeter", "Diameter..ABD.", "Area..ABD.", "Biovolume..Cylinder.", "Biovolume..P..Spheroid.", "Biovolume..Sphere.",
+                             "Volume..ABD.", "Date", "Image.File")] 
+  return(selected_columns)
+} 
+
+#apply columns_to_import function to full D1 data csv
+D1data_filt <- columns_to_import(D1data) #new dataframe containing day 1 data with only necessary columns
+
+#calculate summary statistics for key variables
+calculate_summary_statistics <- function(df) {
+  df %>%
+    group_by(Day, Treatment, Tube) %>%
+    #this keeps these columns in the final df
+    summarize(
+      across(
+        c(Diameter..ABD., Area..ABD., Biovolume..P..Spheroid., Volume..ABD.),
+        list(
+          Mean = ~mean(.x, na.rm = TRUE),
+          Median = ~median(.x, na.rm = TRUE),
+          SD = ~sd(.x, na.rm = TRUE),
+          Range = ~max(.x, na.rm = TRUE) - min(.x, na.rm = TRUE)
+        ),
+        .names = "{col}_{fn}"
+      ),
+      .groups = "drop"
+    )
+}
+
+#apply function to full data df to get summary stats by replicate
+D1data_summary <- calculate_summary_statistics(D1data_filt)
+
+####Filtering aggregate data####
+
+#filter day one's data to include only particles with >80 convex perimeter 
+ag_D1_full <- filter(D1data_filt, Convex.Perimeter >= 80)
+
+# Function to calculate total number of aggregates
+calculate_ag_count_by_treatment <- function(df) {
+  summary_df <- df %>%
+    group_by(Day, Treatment, Tube, Replicate) %>%
+    summarize(
+      total_aggregates = n(), # Count number of rows (aggs) in each group
+      .groups = "drop"
+    )
+  return(summary_df)
+}
+
+#calculate aggregate count
+D1_ag_count <- calculate_ag_count_by_treatment(ag_D1_full)
+
+#define new Treatment labels for graphs
+new_labels <- c("cn" = "Control", "cntw" = "Control + Tween", "low" = "Low MP", "med" = "Medium MP", "high" = "High MP")
+
+#plot of aggregate count on day 1 by treatment
+ggplot(D1_ag_count, aes(x=Treatment, y=total_aggregates, fill = Treatment)) +
+  geom_boxplot(outliers = F) +
+  geom_jitter(shape = 21, size = 1.5, colour = "black", alpha = 0.5) +
+  theme_minimal()+
+  scale_fill_manual(values = c("cn" = "white", "cntw" = "darkgrey", "low" = "lightblue", "med" = "deepskyblue", "high" = "blue"), labels = new_labels) +
+  labs(title = "Aggregate count by treatment", y = "Number of aggregates per replicate")
--- 01_scripts/rawdata_saving.R
+++ 01_scripts/rawdata_saving.R
@@ -3,7 +3,7 @@
 library(dplyr)
 
 # List all CSV files in the D1_filteredcsv folder
-D1_files <- list.files(path = "../../Tseng Lab/MP Directed Studies/D1_filteredcsv", pattern = "\\.csv$", full.names = TRUE)
+D1_files <- list.files(path = "/Users/katie/Documents/Tseng Lab/MP Directed Studies/D1_filteredcsv", pattern = "\\.csv$", full.names = TRUE)
 
 # Read all CSV files into a list and add a column with the file name
 D1_data_list <- lapply(D1_files, function(file) {
@@ -13,7 +13,48 @@ D1_data_list <- lapply(D1_files, function(file) {
 })
 
 #combine individual dataframes into one large csv file
-full_D1_data <- bind_rows(D1_data_list)
+D1data <- bind_rows(D1_data_list)
+
+####Add columns to show sample info (day, treatment, tube #, replicate #)####
+# do this by subsetting the 'filename' column info
+
+# Use extract to split the column into new columns -- use [a-zA-Z] for letters and (\\d+) for numbers
+
+add_filename_columns <- function(df) {
+  df <- extract(df, filename,
+                into = c("Day", "Treatment", "Tube", "Replicate"),
+                regex = "(D\\d+)([a-zA-Z]+)(\\d{1})_(\\d{1})",
+                remove = FALSE)
+  return(df)
+}
+
+#apply add filename function
+D1data <- add_filename_columns(D1data)
+
+####ERRORS HERE#### 
+#move columns to the front & rename treatment groups
+D1data <- relocate(D1data, c("Day", "Treatment", "Tube", "Replicate"), .before = "Particle.ID") %>% 
+  mutate(Treatment = factor(Treatment, levels = c("cn", "cntw", "low", "med", "high"))) %>%
+  arrange(Treatment)
+
+#rename treatment groups
+levels(Treatment) <- c("cn" = "Control",
+                       "cntw" = "Control + Tween",
+                       "low" = "Low",
+                       "med" = "Medium",
+                       "high" = "High")
+
+
+#reorder treatment groups so "high" is last
+D1data <- D1data %>%
+  mutate(Treatment = factor(Treatment, levels = c("cn", "cntw", "low", "med", "high"))) %>%
+  arrange(Treatment)
+
+##filter to only one biological replicate per treatment (tube #1) to make csv file small enough for github! 
+D1data_tube1 <- D1data %>% 
+  group_by()
+  filter()
+
 
 #save this csv file to LDP rawdata folder
 write.csv(full_D1_data, file = "00_rawdata/D1data.csv", row.names = FALSE)
